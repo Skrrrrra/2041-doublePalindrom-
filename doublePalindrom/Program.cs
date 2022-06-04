8637dd049b838b2f140af792dbbc65864a8581e5
kseni <kseni@DESKTOP-AHVNS4G>
2022-06-05 02:25:13 +0300

    Добавьте файлы проекта.

---
+++ doublePalindrom/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace palindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //путь
+            string inputpath = "D:\\solutionsForSpaceApp\\2041\\input.txt";
+            string outputpath = "D:\\solutionsForSpaceApp\\2041\\output.txt";
+
+            //создание файлов
+            FileStream fs = new FileStream(inputpath, FileMode.OpenOrCreate);
+            fs.Close();
+            FileStream fsout = new FileStream(outputpath, FileMode.OpenOrCreate);
+            fsout.Close();
+            
+            //запись в строку содержимогостроки файла
+            string secondLine;
+            using (var readersecond = new StreamReader(inputpath))
+            {
+                secondLine = readersecond.ReadLine();  // записываем в переменную
+            }
+
+            
+
+            List<string> A = new List<string>();
+            List<string> B = new List<string>();
+
+            foreach (var line in secondLine)
+            {
+                A.Add(Convert.ToString(line));
+                B.Add(Convert.ToString(line));
+            }
+
+            B.Reverse();
+
+
+
+            double middle = A.Count/2;
+
+            if(A.Count /2 !=0)
+            {
+                middle = Math.Floor(middle);
+            }
+            List<int> output = new List<int>();
+            for (int i = 0; i <= middle; i++)
+            {
+                if (A[i] == B[i])
+                {
+                    output.Add(1);
+                }
+                else
+                {
+                    output.Add(0);
+                }
+            }
+            string yeah = "Yes";
+            string nope = "No";
+            if(output.Contains(0))
+            {
+                File.WriteAllText(outputpath,nope);
+            }
+            else 
+            {
+                File.WriteAllText(outputpath, yeah);
+            }
+            
+        }
+    }
+}
---
+++ doublePalindrom/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("doublePalindrom")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("doublePalindrom")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("33b8d6e3-7779-4825-a88b-c7ec6593f13a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
